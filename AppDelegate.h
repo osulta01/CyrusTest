fafc3302617d1af9c5fa3e21e799da1eb58f7006
Ohida Sultana <osulta01@students.poly.edu>
2015-07-31 15:37:03 -0400

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CyrusTest
+//
+//  Created by ohida sultana on 7/29/15.
+//  Copyright (c) 2015 WorldPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
